9b32ae3aaf876fddb4e8639364e8aad20ba7e526
Ezequiel Torres <torreszeaks@gmail.com>
2024-06-23 22:44:41 -0700

    Adding chapter 5 and 6.

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-mySet = {"apple", "banana", "cherry"}
-mySet.remove("apple")
-if "apple" not in mySet:
-    print("There are no apples in the set")
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def increaseAge(self):
+        self.age += 1
+class GoldenRetriever(Dog):
+    def __init__(self, name, age, furColor):
+
+        super().__init__(name, age)
+
+        self.furColor = furColor
+
+myDog = GoldenRetriever("Lucky", 2, "Gold")
+
+myDog.increaseAge()
+
+print(myDog.age)
+
+print(myDog.furColor)
+myDog = Dog("Lucky", 3)
+myDog.increaseAge() 
+print(myDog.age)
